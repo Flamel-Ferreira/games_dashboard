e80da44348c090d8c5d22eb45cc2351a1715e70b
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-05 13:34:33 -0300

    Feat: Testing Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { StyledBaseTitle, StyledParagraph } from './styles/typography'
 
 function App() {
   return (
     <div className='App'>
-      
+        <StyledBaseTitle 
+            tag='h1' 
+            fontWeight={400} 
+            sizeCategory='lg' 
+            className='titulo'
+        >Games Dashboard</StyledBaseTitle>
+
+        <StyledParagraph sizeCategory='lg' >Parágrafo</StyledParagraph>
     </div>
   )
 }
