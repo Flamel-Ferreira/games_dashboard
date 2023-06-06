d4c4d2de302e305f14824415fb9eefb5e6d0a3bf
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-06 12:21:10 -0300

    Feat: Adding ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { GlobalStyle } from './styles/global.ts'
+import { ThemeProvider } from 'styled-components'
+import { ColorsTheme } from './styles/colorTheme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <ThemeProvider theme={ColorsTheme}>
+        <App />
+    </ThemeProvider>
+        
   </React.StrictMode>,
 )
