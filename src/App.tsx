bf3a4c2ec40ca69715dff17e8ff9d67a6fda5682
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-08 18:15:05 -0300

    Feat: Adding Header in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import './App.css'
 import { StyledButton } from './styles/buttons'
+import HeaderComponent from './styles/components/Header/HeaderComponent'
 import { StyledBaseTitle, StyledParagraph } from './styles/typography'
 
 function App() {
   return (
     <div className='App'>
-        <StyledBaseTitle 
-            tag='h1' 
-            fontWeight={400} 
-            sizeCategory='lg' 
-            className='titulo'
-        >
-            Games Dashboard
-        </StyledBaseTitle>
-
-        <StyledParagraph 
-            sizeCategory='lg' 
-        >
-            Parágrafo
-        </StyledParagraph>
-
-        <StyledButton 
-            buttonSize='sm'
-            buttonStyle='outline'
-            className='infoButton'
-        >
-            Ver Mais
-        </StyledButton>
+        <HeaderComponent />
     </div>
   )
 }
