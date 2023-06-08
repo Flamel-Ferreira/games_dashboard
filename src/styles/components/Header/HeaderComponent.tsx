1b8fce2d59fbecbcbb11b4b89a3dec800c8d8fad
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-08 18:14:45 -0300

    Feat: Creating Header

---
+++ src/styles/components/Header/HeaderComponent.tsx
@@ -0,0 +1,24 @@
+import {MdSearch, MdNotifications } from 'react-icons/md'
+import Avatar1 from '../../../assets/Avatar 1.png'
+import { StyledHeader, StyledHeaderForm } from './StyledHeaderComponent'
+
+const HeaderComponent = () => {
+  return (
+    <StyledHeader>
+        <StyledHeaderForm>
+            <button type='submit'>
+                <MdSearch size={30} color='rgba(255,255,255,.5)'/>
+            </button>
+            <input type="text" name='search' id='search' placeholder='Buscar...'/>
+        </StyledHeaderForm>
+        <div className="userMenu">
+            <button>
+                <MdNotifications size={30} color='#FFF' />
+            </button>
+                <img src={Avatar1} alt="Nome do Usuário" />
+        </div>
+    </StyledHeader>
+  )
+}
+
+export default HeaderComponent
