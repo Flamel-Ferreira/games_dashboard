8eaad12f52b82918168bae00832857150a61283d
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-08 18:13:51 -0300

    Feat: Adding color in ColorTheme

--- src/styles/colorTheme.ts
+++ src/styles/colorTheme.ts
@@ -2,6 +2,7 @@ export const ColorsTheme = {
     colors:{
         white: '#FFFFFF',
         white20: 'rgb(217,217,217,0.15)',
+        white50: 'rgb(255,255,255,0.5)',
         red:'#F55859',
         blue: '#0F81EC',
     },
