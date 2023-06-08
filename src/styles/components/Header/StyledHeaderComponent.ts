924f2522bec6f31d72596966d9d9b1ecbf422526
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-08 18:14:31 -0300

    Feat: Creating Components for Header

---
+++ src/styles/components/Header/StyledHeaderComponent.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    margin-top: .5rem;
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+    gap: 1rem;
+
+    .userMenu{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        img{
+            width:60px;
+            height:60px;
+            object-fit:cover;
+        }
+    }
+
+    @media (max-width: 600px){
+         flex-direction: column-reverse;
+
+         .userMenu{
+            width:100%;
+            justify-content:flex-end;
+         }
+    }
+`
+
+export const StyledHeaderForm = styled.form`
+    display: inline-flex;
+    align-items: center;
+    height:60px;
+    max-width:370px;
+    width: 100%;
+    padding: 0 1rem;
+    gap: .5rem;
+    border-radius:10px;
+    background-color: ${({theme}) => theme.colors.white20};
+
+    input{
+        font-family: 'Poppins',sans-serif;
+        font-size:16px;
+        color: ${({theme}) => theme.colors.white};
+        background: transparent;
+        border:none;
+        max-width: 370px;
+        width: 100%;
+
+        &::placeholder{
+            font-weight:bold;
+            color: ${({theme}) => theme.colors.white50};
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+`
