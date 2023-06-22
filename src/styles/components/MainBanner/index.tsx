6b4e9a7218cb323d6d530c6711858b357f989ef8
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-22 10:56:39 -0300

    Feat: Creating MainBanner Component

---
+++ src/styles/components/MainBanner/index.tsx
@@ -0,0 +1,19 @@
+import { StyledBaseTitle } from "../../typography"
+
+
+const MainBanner = () => {
+  return (
+    <div>
+        <div className="flexGrid">
+            <StyledBaseTitle tag='h2' fontWeight={400} fontSize="lg">
+                Pocket Monster Fantasy World 
+            </StyledBaseTitle>
+        </div>
+        <div className="imageGrid">
+
+        </div>
+    </div>
+  )
+}
+
+export default MainBanner
