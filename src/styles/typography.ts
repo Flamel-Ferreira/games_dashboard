f13c958d689ee417a0d7dab02f5c8119a0d56e57
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-22 10:56:04 -0300

    Feat: Fixing prop name

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,17 +1,18 @@
-import styled, {css} from "styled-components";
+import styled, {DefaultTheme, css} from "styled-components";
 import BaseTitle from "./components/typography";
 
 interface iStyledBaseTitleProps{
     fontWeight: 400 | 700,
-    sizeCategory: 'lg' | 'md' | 'sm',
+    fontSize: 'lg' | 'md' | 'sm',
     
 }
 
 export const StyledBaseTitle = styled(BaseTitle)<iStyledBaseTitleProps>`
+    color: ${({theme}) => theme.colors.white};
     font-family: 'Popping', sans-serif;
     font-weight: ${({fontWeight})=> fontWeight};
-    font-size: ${({sizeCategory})=> {
-        switch(sizeCategory){
+    font-size: ${({fontSize})=> {
+        switch(fontSize){
             case 'lg':
                 return '44px'
             case 'md':
@@ -25,17 +26,19 @@ export const StyledBaseTitle = styled(BaseTitle)<iStyledBaseTitleProps>`
 // Outra sintaxe de lógica de variantes com switch case, melhor para quando ouver mais de uma propriedade para estilizar com base em uma única props
 interface iStyledParagraph{
     opacity?: number;
-    sizeCategory: 'lg'|'md'|'sm';
+    fontSize: 'lg'|'md'|'sm';
     children: React.ReactNode,
+    theme: DefaultTheme;
 }
 
 export const StyledParagraph = styled.p<iStyledParagraph>`
+    color: ${({theme}) => theme.colors.white};
     font-family: 'Poppings', sans-serif;
     font-weight: 400;
     opacity: ${({opacity}) => opacity ? opacity : .5};
 
-    ${({sizeCategory}) => {
-        switch(sizeCategory){
+    ${({fontSize}) => {
+        switch(fontSize){
             case 'lg':
                 return css`
                     font-size:20px;
