09b11e61cd7b2845ab9137df562dc55309e99623
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-23 10:52:53 -0300

    Feat: Styling MainBanner

---
+++ src/styles/components/MainBanner/Style.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+export const StyledBanner = styled.section`
+    display:flex;
+    gap: 16px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px 40px;
+    border-radius: 32px;
+    background-color: gray;
+    background: rgb(44,43,43);
+    background: radial-gradient(circle, rgba(44,43,43,.3) 10%, rgba(211,55,55,.5) 100%);
+
+    @media (max-width: 768px){
+        flex-direction: column-reverse;
+        justify-content: center;
+        align-items: center;
+        border-radius: 12px;
+    }
+    
+`
+
+export const FlexGrid = styled.div`
+    max-width:350px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 16px;
+
+    @media (max-width: 768px){
+        align-items:center;
+        text-align: center;
+    }
+`
+
+export const TagsGrid = styled.div`
+    display: inline-flex;
+    flex-wrap: wrap;
+    gap: 16px;
+
+    @media (max-width: 768px){
+        justify-content: center;
+        gap:8px;
+    }
+`
+export const PriceBox = styled.div`
+    margin-top:32px;
+    background-color: rgba(217,217,217,.15);
+    display: inline-flex;
+    align-items: center;
+    width: fit-content;
+    border-radius: 12px;
+
+    p{
+        padding: 10px 20px;
+    }
+
+    @media (max-width: 380px){
+        flex-direction: column;
+        margin-top:10px;
+    }
+`
+
+export const ImageGrid = styled.div`
+    
+    img{
+        max-width:100%;
+        object-fit: cover;
+    }
+`
--- src/styles/components/MainBanner/index.tsx
+++ src/styles/components/MainBanner/index.tsx
@@ -1,29 +1,33 @@
 import { StyledButton } from "../../buttons"
 import { StyledBaseTitle, StyledParagraph, StyledTag } from "../../typography"
 import Charizard from '../../../assets/Charizard.png'
+import { StyledBanner, FlexGrid,TagsGrid,PriceBox, ImageGrid } from "./Style"
 
 const MainBanner = () => {
   return (
-    <div>
-        <div className="flexGrid">
-            <StyledTag backgroundColor="red" >RPG</StyledTag>
-            <StyledTag backgroundColor="blue" >AVENTURA</StyledTag>
+    <StyledBanner>
+        <FlexGrid>
+            <TagsGrid>
+                <StyledTag backgroundColor="red" >RPG</StyledTag>
+                <StyledTag backgroundColor="blue" >AVENTURA</StyledTag>
+            </TagsGrid>
             <StyledBaseTitle tag='h2' fontWeight={400} fontSize="lg">
                 Pocket Monster Fantasy World
             </StyledBaseTitle>
-        </div>
-        <div className="priceBox">
-            <StyledParagraph fontSize='lg' opacity={1}>
-                <strong>
-                    R$ 299,00
-                </strong>
-            </StyledParagraph>
-            <StyledButton buttonSize='lg' buttonStyle='solid' >Comprar</StyledButton>
-        </div>
-        <div className="imageGrid">
+            <PriceBox>
+                <StyledParagraph fontSize='lg' opacity={1}>
+                    <strong>
+                        R$ 299,00
+                    </strong>
+                </StyledParagraph>
+                <StyledButton buttonSize='lg' buttonStyle='solid' >Comprar</StyledButton>
+            </PriceBox>
+        </FlexGrid>
+        
+        <ImageGrid>
             <img src={Charizard} alt="Imagem de Charizard" />
-        </div>
-    </div>
+        </ImageGrid>
+    </StyledBanner>
   )
 }
 
