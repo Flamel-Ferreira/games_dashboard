e9562074ebfb5e5a4ca0b1bfc75868335b6d1186
Flamel-Ferreira <flamelferreira05@hotmail.com>
2023-06-29 09:44:42 -0300

    Feat: Structuring MostPlayed Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import HeaderComponent from './styles/components/Header/HeaderComponent'
 import MainBanner from './styles/components/MainBanner'
+import MostPlayed from './styles/components/MostPlayed'
 
 function App() {
   return (
     <div className='App'>
         <HeaderComponent />
         <MainBanner />
+        <MostPlayed />
     </div>
   )
 }
--- src/styles/components/MostPlayed/index.tsx
+++ src/styles/components/MostPlayed/index.tsx
@@ -1,6 +1,40 @@
+import { MdArrowForward } from "react-icons/md"
+import { StyledBaseTitle } from "../../typography"
+
 const MostPlayed = () => {
   return (
-    <div>index</div>
+    <div className="Flex">
+        <div className="header">
+            <StyledBaseTitle fontSize="md" fontWeight={700} tag="h3">
+                Os mais jogados
+            </StyledBaseTitle> 
+            <MdArrowForward/>
+        </div>
+
+        <div className="Flex">
+            <div className="box-container">
+                <img src="" alt="" />
+                <StyledBaseTitle fontSize="sm" fontWeight={700} tag="h4">
+                    Thie Lorem Game
+                </StyledBaseTitle>
+                <p>Lorem Company</p>
+            </div>
+            <div className="box-container">
+                <img src="" alt="" />
+                <StyledBaseTitle fontSize="sm" fontWeight={700} tag="h4">
+                    Thie Lorem Game
+                </StyledBaseTitle>
+                <p>Lorem Company</p>
+            </div>
+            <div className="box-container">
+                <img src="" alt="" />
+                <StyledBaseTitle fontSize="sm" fontWeight={700} tag="h4">
+                    Thie Lorem Game
+                </StyledBaseTitle>
+                <p>Lorem Company</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
